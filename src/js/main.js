95eb7da7b86f6f8e9a3cfd8672c0dd3492108e3e
coldplay520 <1849641432@qq.com>
2017-08-09 10:55:08 +0800

    update

---
+++ src/js/main.js
@@ -0,0 +1,22 @@
+$(function(){
+	$(".owl-carousel").owlCarousel({
+		items: 1,
+		loop: true,
+		autoplay: true,
+		autoplayTimeout: 1000,
+		autoplayHoverPause: true
+	});
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
